8f975e4cfe785265450fd90a3e9690cf1cfb09a2
jwzzang12 <moonbb12@gmail.com>
2023-07-09 20:57:08 +0100

    Change style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import "./App.css";
-import "./css/layout.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import New from "./components/New";
-import List from "./components/List";
-import { useSelector } from "react-redux";
+import './css/layout.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import New from './components/New';
+import List from './components/List';
+import { useSelector } from 'react-redux';
 
 function App() {
   const data = useSelector((state) => state.dataList);
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <New />
       <List data={data} />
